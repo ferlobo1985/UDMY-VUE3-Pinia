5c2aca3ff1c775b1429b26bfa693c39c4aee2f97
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-09-28 17:58:19 -0300

    02-Mutations
    02-Mutations

--- src/components/admin/index.vue
+++ src/components/admin/index.vue
@@ -4,25 +4,60 @@
       <hr />
       <div class="col-sm-5">
         <p>
-          <strong> Change count:0 </strong>
+          <strong> Change count: {{ count }} </strong>
         </p>
   
-        <div>
+        <!-- <div>
           <label class="form-label">By how much</label>
           <input type="number" class="form-control" v-model="data.amount" />
         </div>
-        <br />
+        <br /> -->
   
-        <button type="button" class="btn btn-primary me-3">+</button>
-        <button type="button" class="btn btn-outline-secondary">-</button>
+        <button type="button" class="btn btn-primary me-3"
+          @click="add"
+        >+</button>
+        <button type="button" class="btn btn-outline-secondary"
+         @click="subtract"
+        >-</button>
       </div>
+      <div>
+        <hr/>
+        <p>{{ store.car.brand }}</p>
+        <p>{{ store.car.color }}</p>
+        <button
+          type="button"
+          class="btn btn-outline-secondary"
+          @click="patchState"
+        >
+          Patch state
+        </button>
+      </div>
+
     </div>
   </template>
   
-  <script setup>
-    import { reactive } from 'vue'
-  
-    const data = reactive({
-      amount:1
+<script setup>
+  import { computed } from 'vue';
+  import { useCounterStore } from '@/store/counter'
+
+  const store = useCounterStore();
+  const count = computed(()=> store.counter);
+
+  const add = () => {
+    store.counter++
+  }
+  const subtract = () => {
+    store.counter--
+  }
+
+  const patchState = () => {
+    store.$patch({
+      counter:100,
+      car:{
+        brand:'Ford',
+        color:'Ranger'
+      }
     })
-  </script>
+  }
+
+</script>
--- src/store/counter.js
+++ src/store/counter.js
@@ -3,6 +3,10 @@ import { defineStore } from "pinia";
 
 export const useCounterStore = defineStore('counter',{
     state:()=>({
-        counter:7
+        counter:7,
+        car:{
+            brand:'',
+            color:''
+        }
     })
 })
